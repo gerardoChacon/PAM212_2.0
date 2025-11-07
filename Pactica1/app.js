e34ff7148fe029c9c06770434016cb47b3b27b7c
Gerardo Chacón Álvarez <gchaconalvarez@hotmail.com>
2025-11-06 21:32:22 -0600

    Practica 1 finalizada

---
+++ Pactica1/app.js
@@ -0,0 +1,13 @@
+// Variables
+const nombre = "Gerardo";
+let edad = 27;
+
+let saludo =  `Hola, ${nombre}. Tienes ${edad} años`;
+
+//Funciones
+/* 
+function cuadrdo(numero){
+    return numero * numero;
+} */
+
+const cuadrado = numero => numero * numero;
