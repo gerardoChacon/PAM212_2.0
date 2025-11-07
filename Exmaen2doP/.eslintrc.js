e8df8cf6b372a864317bea9d183bb12727ed026a
Gerardo Chacón Álvarez <gchaconalvarez@hotmail.com>
2025-11-07 16:25:32 -0600

    Creando proyecto para el examen del 2do parcial

---
+++ Exmaen2doP/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', 'react-native', 'prettier'],
+  env: {
+    es6: true,
+    node: true,
+    'react-native/react-native': true,
+  },
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'prettier/prettier': ['error'],
+    'react/react-in-jsx-scope': 'off',
+  },
+};
